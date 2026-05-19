d7ae5ea01ad2c3a8d47a679fa0ed3df7d435b768
Willians DSR <willians@willians.com.br>
2026-05-19 20:49:03 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(_name_)
+
+VERIFY_TOKEN = "codeclub123"
+
+@app.route("/webhook", methods=["GET"])
+def verify():
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+
+    if token == VERIFY_TOKEN:
+        return challenge
+
+    return "Token inválido", 403
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    print(data)
+    return "ok", 200
+
+@app.route("/")
+def home():
+    return "CodeClub Bot Online"
